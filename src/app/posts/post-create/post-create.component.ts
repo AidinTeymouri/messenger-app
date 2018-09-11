8f2b4811470ac9a804b4a4929a8531af15bbf943
Aidin <aidintv@gmail.com>
2018-09-11 19:18:51 +1000

    part 1 - Angular

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { PostsService } from "../posts.service";
 
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
-export class PostCreateComponent implements OnInit {
-  enteredValue = '';
-  newPost = '';
-  constructor() { }
+export class PostCreateComponent {
+  enteredTitle = "";
+  enteredContent = "";
 
-  ngOnInit() {
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
-
-  onAddPost() {
-    console.log(this.enteredValue);
-    this.newPost = this.enteredValue;
-  }
-
 }
